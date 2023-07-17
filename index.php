a515b392df072d5e2bfd479b723f37e21facdc05
金誠俊 <kimseongjun@kim.local>
2023-07-17 15:44:22 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
+    <title></title>
+    <meta name="description" content="">
+    <link rel=”canonical” href=”https://www.exsample.com/aaa/”>
+    <link rel="icon" href="asset/img/common/favicon.svg" type="image/svg+xml">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.css">
+    <link rel="stylesheet" href="asset/css/top/style.css">
+    <!-- OGP設定 -->
+    <meta property="og:url" content="ページのURL">
+    <meta property="og:title" content="ページのタイトル">
+    <meta property="og:type" content="website">
+    <meta property="og:description" content="">
+    <meta property="og:image" content="画像のURL">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="og:site_name" content="サイト名">
+    <meta property="og:locale" content="ja_JP">
+    <!-- OGP設定 -->
+</head>
+
+<body class="top">
+    <!-- <div class="modal-overlay">
+    <div class="modal-panel">
+      <iframe width="928" height="522" src="" frameborder="0" allowfullscreen id="videoframe"></iframe>
+    </div>
+    <div class="modal-close"><span></span></div>
+  </div> -->
+    <!-- <div class="fix_bg_wrap">
+    <div class="fix_bg ablt"></div>
+  </div> -->
+    <div id="stage">
+        <div class="content">
+            <div class="header">
+
+            </div>
+            <!-- <?php $Path = "./"; require('asset/inc/header.php'); ?> -->
+            <div class="firstview"></div>
+            <div class="footer">
+
+            </div>
+            <!-- <?php $Path = "./"; require('asset/inc/footer.php'); ?> -->
+        </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js"
+        integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" crossorigin="anonymous">
+    </script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.9.0/slick.min.js"></script>
+    <script src="asset/js/common.js"></script>
+</body>
+
+</html>
